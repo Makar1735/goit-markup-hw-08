df593601532d6ea9b57d15f4100f3b041787ad7d
Makar <dojervip1@gmail.com>
2022-11-07 18:03:20 +0200

    HW test button

--- js/menu.js
+++ js/menu.js
@@ -22,3 +22,24 @@
       refs.body.classList.remove('no-scroll');
     }
   })();
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//    var modalButtons = document.querySelectorAll('.js-open-modal'),
+//        overlay      = document.querySelector('#overlay-modal'),
+//        closeButtons = document.querySelector('.js-modal-close');
+   
+   
+//    modalButtons.forEach(function(item){
+      
+//       item.addEventListener('click', function(e) {
+         
+//          e.preventDefault();
+//          var modalId = this.getAttribute('data-modal'),
+//              modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+         
+//          modalElem.classList.add('active');
+//          overlay.classList.add('active');
+//       }); // end click
+//    }); // end foreach
+// }); // end ready
